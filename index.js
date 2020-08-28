91eab698853e078e05311227f978ec83578d2b21
wshangguan <wenjing.shangguan@sv.cmu.edu>
2020-08-28 12:08:36 -0700

    callbacks + error handling

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var rect = require('./rectangle');
+const rectangle = require('./rectangle');
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b)
-    if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be positive: l = " + l + ", and b = " + b)
-    } else {
-        console.log("The area of the rectangle is " + rect.area(l,b))
-        console.log("The perimeter of the rectangle is " + rect.perimeter(l,b))
-    }
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+            console.log("ERROR: ", err.message);
+        } else {
+            console.log("Area when l = " + l + " and b = " + b + " is " + rectangle.area());
+            console.log("Perimeter when l = " + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement is after call to rectangle.")
 }
 
 solveRect(2,4);
--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,26 @@
-exports.perimeter = (x,y) => (2*(x+y));
-exports.area = (x,y) => (x*y);
+module.exports = (x,y,callback) => {
+    if (x <= 0 || y <= 0) {
+        setTimeout(() => 
+            callback(new Error("Rectangle dimensions should be positive: l = " + x + ", and b = " + y),
+            null),
+            2000);
+    } else {
+        setTimeout(() => 
+            // first param is ERROR
+            callback(null,
+            {
+                // javascript objects, which are fns,
+                // passes as return value of callback fn
+                perimeter: () => (2*(x+y)),
+                area: () => (x*y)
+            }),
+            2000);    }
+}
+
+
+
+
+
+
+
 
